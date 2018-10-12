260b8c3d2ce045bd95e876ec0ba07822b4eb223e
Spencer Schurk <spencer@joyrun.com>
2018-10-12 13:46:47 -0700

    Finished STUDENTS modify script

--- STUDENTS/STUDENTS-modify.sql
+++ STUDENTS/STUDENTS-modify.sql
@@ -21,4 +21,34 @@ WHERE (LASTNAME = 'FLACHS' AND FIRSTNAME = 'JEFFREY')
 OR (LASTNAME = 'HUANG' AND FIRSTNAME = 'TAWANNA')
 OR (LASTNAME = 'GRUNIN' AND FIRSTNAME = 'EMILE');
 
---
+-- Set the GPA of sixth graders to 3.25
+
+UPDATE LIST SET GPA = 3.25 WHERE GRADE = 6;
+
+-- Set the GPA of fifth graders from room 109 to 2.9
+
+UPDATE LIST SET GPA = 2.9 WHERE GRADE = 5 AND CLASSROOM = 109;
+
+-- Set the GPA of fifth graders from room 120 to 3.5
+
+UPDATE LIST SET GPA = 3.5 WHERE GRADE = 5 AND CLASSROOM = 120;
+
+-- Set the GPA of CHET MACIAG to 4.0
+
+UPDATE LIST SET GPA = 4.0 WHERE LASTNAME = 'MACIAG' AND FIRSTNAME = 'CHET';
+
+-- Set the GPA of AL GERSTEIN to be 0.3 higher than whatever it currently is
+
+UPDATE LIST SET GPA = GPA + 0.3 WHERE LASTNAME = 'GERSTEIN' AND FIRSTNAME = 'AL';
+
+-- Set the GPAs of TAWANNA HUANG and ELVIRA JAGNEUX to be 10% higher than their current GPAs.
+
+UPDATE LIST SET GPA = GPA * 1.1
+WHERE (LASTNAME = 'HUANG' AND FIRSTNAME = 'TAWANNA')
+OR (LASTNAME = 'JAGNEUX' AND FIRSTNAME = 'ELVIRA');
+
+-- Complete the script with these two queries
+
+SELECT * FROM LIST ORDER BY GPA, GRADE, LASTNAME;
+
+SELECT * FROM TEACHERS;
