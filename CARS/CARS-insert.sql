7f724a123dddeb4282a45069fa94a45f584217ac
jgoyette <jgoyette@csc301x17.ad.calpoly.edu>
2018-10-12 13:37:01 -0700

    CARS modify and insert created

---
+++ CARS/CARS-insert.sql
@@ -0,0 +1,14 @@
+-- Jack Goyette, Spencer Schurk
+-- jgoyette@calpoly.edu, sschurk@calpoly.edu
+
+source CARS-build-car-makers.sql;
+
+source CARS-build-car-names.sql;
+
+source CARS-build-cars-data.sql;
+
+source CARS-build-continents.sql;
+
+source CARS-build-countries.sql;
+
+source CARS-build-model-list.sql;
